e44af3f26f821acdcea4160d06d8412bbcad0bae
Anthony Liu <anthonyscottliu@gmail.com>
2018-07-28 11:35:58 -0700

    Kind of add paragraphs.

--- public/index.js
+++ public/index.js
@@ -4,12 +4,22 @@ const ReactDOM = require('react-dom');
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { sizeInPx: 16 };
+    this.state = { sizeInPx: 24, pSizeInPx: 16 };
 
     const SpeechRecognition = webkitSpeechRecognition;
     const SpeechGrammarList = webkitSpeechGrammarList;
     const SpeechRecognitionEvent = webkitSpeechRecognitionEvent;
-    const colors = ['make', 'this', 'header', 'even', 'bigger', 'smaller', 'a', 'little'];
+    const colors = [
+      'make',
+      'this',
+      'header',
+      'paragraph',
+      'even',
+      'bigger',
+      'smaller',
+      'a',
+      'little',
+    ];
     const grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;';
     const recognition = new SpeechRecognition();
     const speechRecognitionList = new SpeechGrammarList();
@@ -24,10 +34,12 @@ class App extends React.Component {
       const color = event.results[last][0].transcript;
       console.log('Color: ' + color + '\nConfidence: ' + event.results[0][0].confidence);
 
-      if (color.endsWith('bigger')) {
-        this.setState({ sizeInPx: this.state.sizeInPx * 1.5 });
+      const bigger = color.endsWith('bigger');
+      const multiplier = bigger ? 1.5 : 1 / 1.5;
+      if (color.indexOf('paragraph') === -1) {
+        this.setState({ pSizeInPx: this.state.pSizeInPx * multiplier });
       } else {
-        this.setState({ sizeInPx: this.state.sizeInPx / 1.5 });
+        this.setState({ sizeInPx: this.state.sizeInPx * multiplier });
       }
     };
     recognition.start();
@@ -35,14 +47,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <h1
-        style={{
-          transition: '0.25s',
-          transitionTimingFunction: 'ease-in',
-          fontSize: this.state.sizeInPx + 'px',
-        }}>
-        hello
-      </h1>
+      <div>
+        <h1
+          style={{
+            transition: '0.25s',
+            transitionTimingFunction: 'ease-in',
+            fontSize: this.state.sizeInPx + 'px',
+          }}>
+          Header 1
+        </h1>
+        <p style={{ fontSize: this.state.pSizeInPx + 'px' }}>This is Paragraph 1</p>
+      </div>
     );
   }
 }
